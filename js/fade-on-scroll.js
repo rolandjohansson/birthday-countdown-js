aabac15e9c9d7aec5cc626369bbf4eb0928e39fd
Roland Johansson <hello@rolandjohansson.se>
2018-09-30 17:31:17 +0200

    viewport width checker to toggle fadeOnScroll at right breakpoint

--- js/fade-on-scroll.js
+++ js/fade-on-scroll.js
@@ -1,35 +1,55 @@
-// ===== Fade on Scroll ===== //
+const breakpoint = 960;
 
-// Triggers on document scroll
-$(document).scroll(function() {
+checkViewportWidth();
 
-  // Define var for opacity
-  let alpha = 0;
+$(window).resize(function() {
+  checkViewportWidth();
+});
 
-  // Define var for distance scrolled from top in px
-  let scroll = $(document).scrollTop();
-
-  // At what px to start the fade in
-  const fadeStart = 100;
-
-  // At what px to stop the fade in
-  const fadeStop = 200;
-
-  // Define the length of the fade in
-  const fadeLength = fadeStop - fadeStart;
-
-  // Three cases
-  if (scroll < fadeStart) {
-    alpha = 0;
-  } 
-  // Alpha will go from 0 to 1 when scroll goes between fade start and fade stop
-  else if (scroll < fadeStop) {
-    alpha = (scroll - fadeStart) / fadeLength;
+function checkViewportWidth() {
+  if ($(window).width() < breakpoint) {
+    $("nav").css("background-color", "rgba(43,57,61,1)");
+    console.log("Window narrower than 960px");
+  } else {
+    $("nav").css("background-color", "rgba(43,57,61,0)");
+    fadeOnScroll();
+    console.log("Window wider than 960px");
   }
-  else {
-    alpha = 1;
-  }
+}
 
-  // Set the CSS alpha
-  $("nav").css("background-color", "rgba(43,57,61," + alpha + ")");
-})
+function fadeOnScroll() {
+  // Triggers on document scroll
+  $(document).scroll(function () {
+
+    // Define var for opacity
+    let alpha = 0;
+
+    // Define var for distance scrolled from top in px
+    let scroll = $(document).scrollTop();
+
+    // At what px to start the fade in
+    const fadeStart = 100;
+
+    // At what px to stop the fade in
+    const fadeStop = 200;
+
+    // Define the length of the fade in
+    const fadeLength = fadeStop - fadeStart;
+
+    // Three cases
+    if (scroll < fadeStart) {
+      alpha = 0;
+
+    }
+    // Alpha will go from 0 to 1 when scroll goes between fade start and fade stop
+    else if (scroll < fadeStop) {
+      alpha = (scroll - fadeStart) / fadeLength;
+    }
+    else {
+      alpha = 1;
+    }
+
+    // Set the CSS alpha
+    $("nav").css("background-color", "rgba(43,57,61," + alpha + ")");
+  })
+}
--- js/nav-toggle.js
+++ js/nav-toggle.js
@@ -1,3 +1,8 @@
+//Run navToggle on click on a menu-item
+$("a.menu-item").click(function(){
+  navToggle();
+});
+
 function navToggle() {
   var x = document.getElementById("topnav");
   if (x.className === "") {
